239c77ecb5f02b318be7035deb80c31d218d6cca
ardiaanM <113528715+ardiaanM@users.noreply.github.com>
2022-12-05 11:07:57 +0100

    Added githublink as comment.

--- app/src/main/java/com/kyhskola/smoothfightersproject/MainActivity.kt
+++ app/src/main/java/com/kyhskola/smoothfightersproject/MainActivity.kt
@@ -3,6 +3,9 @@ package com.kyhskola.smoothfightersproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// Github: https://github.com/ardiaanM/SmoothFightersProject
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
