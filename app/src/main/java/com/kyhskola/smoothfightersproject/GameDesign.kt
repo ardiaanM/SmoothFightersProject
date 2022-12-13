f3f13fdb171a02b0b2fd83589c775d7598732bde
ardiaanM <113528715+ardiaanM@users.noreply.github.com>
2022-12-13 17:46:26 +0100

    Daniel och Ardian och Micke.
    - La in debug för att se om posY och posX fungerar.
    - En ny uträkning för player2
    - en ny räkning som gör så att player2 följer efter bollen (utkommenterad)
    - La in playerX2 event!!
    - kommentera ut intersects uträkningen.

--- app/src/main/java/com/kyhskola/smoothfightersproject/GameDesign.kt
+++ app/src/main/java/com/kyhskola/smoothfightersproject/GameDesign.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Rect
-import android.graphics.Rect.intersects
 import android.view.MotionEvent
 import android.view.SurfaceHolder
 import android.view.SurfaceView
@@ -16,11 +15,18 @@ class GameDesign (context: Context): SurfaceView(context), SurfaceHolder.Callbac
     lateinit var canvas:Canvas
     private lateinit var ball1:Ball
     private lateinit var player1: Player1
+    private lateinit var player2: Player2
     private var bounds = Rect()
     var mainActivity = context as MainActivity
     var score = 0
     var mHolder: SurfaceHolder? = holder
     var playerX = 0f
+    var playerX2 = 1f
+
+    /*Hittar skärmens höjd och bredd
+    TODO - Hitta lösning.  */
+    var displayMetrics = context.resources.displayMetrics
+    var screenHeight = displayMetrics.heightPixels
 
 
     init {
@@ -31,11 +37,14 @@ class GameDesign (context: Context): SurfaceView(context), SurfaceHolder.Callbac
     }
 
     fun setup(){
-        ball1 = Ball(100f, 100f, 35f, 20f, 20f)
-        player1 = Player1(30f, 30f, 1f, 0f, 0f, 30f)
+        ball1 = Ball(100f, 100f, 35f, 15f, 15f)
+        player1 = Player1(30f, 30f, 5f, 20f, 0f, 30f)
+        player2 = Player2(300f, 1795f, 1f, 1950f, 0f, 30f)
+
 
         ball1.paint.color = Color.BLACK
         player1.paint.color = Color.RED
+        player2.paint.color = Color.WHITE
     }
 
     fun start(){
@@ -56,12 +65,18 @@ class GameDesign (context: Context): SurfaceView(context), SurfaceHolder.Callbac
     fun update(){
         ball1.update()
         player1.update(playerX)
+        player2.update(playerX2)
 
-        // Check for collisions with the paddle
+        //debug
+        println("Ball position: (${ball1.posX}, ${ball1.posY})")
+        println("Player 1 position: (${player1.posX}, ${player1.posY})")
+        println("Player 2 position: (${player2.posX}, ${player2.posY})")
+
+
+        // Check for collisions with player 1
         if ((ball1.posX < (player1.posX + player1.size)) && ((ball1.posX + ball1.size) > player1.posX) &&
             (ball1.posY < player1.posY + player1.playerHeight) && ((ball1.posY + ball1.size) > player1.posY)
         ) {
-
             // Calculate new direction of the ball
             ball1.speedY = -ball1.speedY
 
@@ -69,6 +84,25 @@ class GameDesign (context: Context): SurfaceView(context), SurfaceHolder.Callbac
             mainActivity.updateText("score: $score")
         }
 
+        // Check for collisions with player 2
+        if ((ball1.posX < (player2.posX + player2.size)) && ((ball1.posX + ball1.size) > player2.posX) &&
+            (ball1.posY < player2.posY + player2.playerHeight) && ((ball1.posY + ball1.size) > player2.posY)
+        ) {
+            // Calculate new direction of the ball
+            ball1.speedY = -ball1.speedY
+
+            score++
+            mainActivity.updateText("score: $score")
+        }
+
+        //bot som följer efter den lilla bollen.
+    // TODO - Fixa skitet.
+        /*if (ball1.posX > player2.posX) {
+            player2.posX += player2.speedX
+        } else if (ball1.posX < player2.posX) {
+            player2.posX -= player2.speedX
+        }
+         */
 
     }
 
@@ -77,12 +111,14 @@ class GameDesign (context: Context): SurfaceView(context), SurfaceHolder.Callbac
         canvas.drawColor(Color.BLUE)
         ball1.draw(canvas)
         player1.draw(canvas)
+        player2.draw(canvas)
         mHolder!!.unlockCanvasAndPost(canvas)
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
 
         playerX = event!!.x
+        playerX2 = event!!.x
         return true
     }
 
@@ -92,14 +128,16 @@ class GameDesign (context: Context): SurfaceView(context), SurfaceHolder.Callbac
         ball1.paint.color = Color.YELLOW
     }
 
-    fun intersects(b1: Ball, p1: Player1){
+
+    // Math pow med intersects, används inte.
+   /* fun intersects(b1: Ball, p1: Player1){
         if (Math.sqrt(Math.pow(b1.posX-p1.posX.toDouble(),2.0)+Math.pow(b1.posY-p1.posY.toDouble(), 2.0))<=b1.size+p1.size){
-            bounceBall(b1,p1)
+           bounceBall(b1,p1)
 
             score++
             mainActivity.updateText("score: $score")
         }
-    }
+    } */
 
 
 
@@ -112,6 +150,7 @@ class GameDesign (context: Context): SurfaceView(context), SurfaceHolder.Callbac
         start()
     }
 
+
     override fun surfaceDestroyed(p0: SurfaceHolder) {
         stop()
     }
@@ -122,6 +161,7 @@ class GameDesign (context: Context): SurfaceView(context), SurfaceHolder.Callbac
             draw()
             //intersects(ball1, player1)
             ball1.checkBounds(bounds)
+
 
         }
     }
