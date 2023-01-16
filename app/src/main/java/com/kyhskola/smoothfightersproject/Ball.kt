3e53f01dd53d596d46bc9ee35c12c52c27d49b7c
Daniel <duszanowiczdaniel@outlook.com>
2023-01-16 21:03:47 +0100

    Efter att man fått 5 poäng kommer man tillbaka till start menyn Använder oss nu av ScreenWidth och height

--- app/src/main/java/com/kyhskola/smoothfightersproject/Ball.kt
+++ app/src/main/java/com/kyhskola/smoothfightersproject/Ball.kt
@@ -1,9 +1,14 @@
 package com.kyhskola.smoothfightersproject
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Rect
+import android.util.DisplayMetrics
+import android.view.MotionEvent
+import android.view.View.OnTouchListener
+import androidx.core.content.ContextCompat.startActivity
 
 
 class Ball(
@@ -15,6 +20,9 @@ class Ball(
     context: Context
 ) {
 
+    var displayMetrics: DisplayMetrics = context.resources.displayMetrics
+    var screenHeight = displayMetrics.heightPixels
+    var screenWidth = displayMetrics.widthPixels
     val paint = Paint()
     var mainActivity = context as MainActivity
     var scoreLower = 0
@@ -38,8 +46,8 @@ class Ball(
             this.speedX *= -1
         }
         if (posY - size < bounds.top) {
-            this.posY = 900f
-            this.posX = 700f
+            this.posY = screenHeight/2f
+            this.posX = screenWidth/2f
             this.speedX *= 0
             this.speedY *= 0
             scoreLower++
@@ -48,8 +56,8 @@ class Ball(
 
         }
         if (posY + size > bounds.bottom) {
-            this.posY = 900f
-            this.posX = 500f
+            this.posY = screenHeight/2f
+            this.posX = screenWidth/2f
             this.speedX *= 0
             this.speedY *= 0
             scoreUpper++
@@ -73,10 +81,18 @@ class Ball(
         if (scoreUpper == 5 && scoreLower != 5){
             mainActivity.updateText("You Lose!")
             mainActivity.updateTextUpper("You Win!")
+            val intent = Intent(mainActivity, StartMenu::class.java)
+            mainActivity.startActivity(intent)
+
         }
         else if (scoreLower == 5 && scoreUpper != 5) {
             mainActivity.updateText("You Win!")
             mainActivity.updateTextUpper("You Lose!")
+            val intent = Intent(mainActivity, StartMenu::class.java)
+            mainActivity.startActivity(intent)
+
         }
+
     }
+
 }
